4ad207ee31a7c7d8c7e9a1a88ca0a5e2bf7fd14e
P1res2 note <pires17.gg@gmail.com>
2025-05-31 05:10:53 -0300

    atualizando-admin/cardapio

--- restaurante/admin.py
+++ restaurante/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from restaurante.models import Cardapio
 
 class ListandoCardapio(admin.ModelAdmin):
-    list_display = ("id", "nome", "categoria", "preco", "imagem")
+    list_display = ("id", "nome", "preco", "categoria", "publicado", "imagem")
     list_display_links = ("id", "nome")
-    search_fields = ("nome",)
     list_filter = ("categoria",)
+    list_editable = ("publicado",)
+    search_fields = ("nome",)
     list_per_page = 10
 
 admin.site.register(Cardapio, ListandoCardapio)
--- restaurante/views.py
+++ restaurante/views.py
@@ -5,5 +5,5 @@ def index(request):
     return render(request, 'restaurante/index.html')
 
 def cardapio(request):
-    cardapio = Cardapio.objects.all()
+    cardapio = Cardapio.objects.order_by("nome").filter(publicado=True)
     return render(request, 'restaurante/cardapio.html', {"cardapio":cardapio})
